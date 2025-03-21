a3666cb701f3c457d56f0e7ba14489a9abc8d49b
Steve Cook <stevenbradleycook@gmail.com>
2025-03-21 19:00:54 -0400

    Update index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,31 @@
-class PostsController < ApplicationController
-  def create
-  end
+# app/controllers/admin/posts_controller.rb
+class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!  # Optional: Add authentication check
 
-  def new
-  end
-
+  # Show the form to edit an existing post
   def edit
+    @post = Post.find(params[:id])
   end
 
+  # Handle the form submission to update the post
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to admin_post_path(@post), notice: "Post updated successfully!"
+    else
+      render :edit
+    end
   end
 
-  def destroy
+  private
+
+  # Strong parameters for the post
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  # Optional: Add authentication logic (e.g., verify admin logged in)
+  def authenticate_admin!
+    # You could check for a session or current_user to verify admin status.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get "posts/create"
-  get "posts/new"
-  get "posts/edit"
-  get "posts/update"
-  get "posts/destroy"
+  resource :session, only: [ :new, :create, :destroy ]
+  resources :posts
   get "home/index"
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
